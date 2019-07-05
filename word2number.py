5b6124188a65219d3ef2bc165d7356466a8f151b
Tongxi Yan <tonkei0361@gmail.com>
2019-07-05 00:18:58 -0400

    Functional word2number.py

---
+++ word2number.py
@@ -0,0 +1,34 @@
+
+def letter2num(cell):
+
+    cell = cell.upper()
+
+    for char in cell:
+        if char in 'ABC':
+            cell = cell.replace(char, '2')
+        elif char in 'DEF':
+            cell = cell.replace(char, '3')
+        elif char in 'GHI':
+            cell = cell.replace(char, '4')
+        elif char in 'JKL':
+            cell = cell.replace(char, '5')
+        elif char in 'MNO':
+            cell = cell.replace(char, '6')
+        elif char in 'PQRS':
+            cell = cell.replace(char, '7')
+        elif char in 'TUV':
+            cell = cell.replace(char, '8')
+        elif char in 'WXYZ':
+            cell = cell.replace(char, '9')
+        else:
+            pass
+    return cell
+
+while True:
+    user_input = input('tyep a number with letters (x to exit):')
+    if user_input == 'x':
+        print('Bye!')
+        break
+    print(letter2num(user_input))
+
+
