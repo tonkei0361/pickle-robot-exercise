470e6ed935e6fc139eda980fb1471f3305bf070b
Tongxi Yan <tonkei0361@gmail.com>
2019-07-12 16:20:01 -0400

    Added one test case to test.py

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import all_wordifications as aw
 import number2word as n2w
 import word2number as w2n
 
-cell_test = ['800QUALITY', '617-HARVARD', '800RECTIFY', '800RIGIDLY',
+cell_test = ['8001EMPIRE', '800QUALITY', '617-HARVARD', '800RECTIFY',
              '2175683968', '8007825439', '8001397328', '8001ROOKIE',
              '8002222345', '8006666666', '2174170499', '8006294000',
              '800-747-9300', '6174797052', '880JACKSON', '9811HEYDOG',
-             '217LD13KSX']
+             '217LD13KSX', '800RIGIDLY']
 
 for cell in cell_test:
     print('Original input:', cell)
