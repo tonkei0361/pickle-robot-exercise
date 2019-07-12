0d664dc0d2f9054a0de7fea45e284669d2ccf82d
Tongxi Yan <tonkei0361@gmail.com>
2019-07-12 16:11:56 -0400

    Added test.py to verify that all three functions are working

--- test.py
+++ test.py
@@ -2,23 +2,19 @@ import all_wordifications as aw
 import number2word as n2w
 import word2number as w2n
 
-# Test number_to_word function
-test_n2w = ['800-747-9300', '8006294000', '6174797052']
-print(test_n2w)
+cell_test = ['800QUALITY', '617-HARVARD', '800RECTIFY', '800RIGIDLY',
+             '2175683968', '8007825439', '8001397328', '8001ROOKIE',
+             '8002222345', '8006666666', '2174170499', '8006294000',
+             '800-747-9300', '6174797052', '880JACKSON', '9811HEYDOG',
+             '217LD13KSX']
 
-for case in test_n2w:
-    wordified = n2w.number_to_words(case)
-    print(wordified)
-    for wordified_numbers_list in wordified.values():
-        for wordified_number in wordified_numbers_list:
-            if n2w.format_cell(w2n.words_to_number(wordified_number)) != n2w.format_cell(case):
-                print('Mismatch detected!')
-
-# Test word_to_number function
-test_w2n = ['880JACKSON', '9811HEYDOG', '800BLABLA1', '217LD13KSX']
-print(test_w2n)
-numeric_list = []
-for case in test_w2n:
-    numeric = w2n.words_to_number(case)
-    numeric_list.append(numeric)
-print(numeric_list)
+for cell in cell_test:
+    print('Original input:', cell)
+    print('Convert to numerical only:', w2n.words_to_number(cell))
+    print('Single wordification:')
+    for key, value in n2w.number_to_words(w2n.words_to_number(cell)).items():
+        print(key, ':', value)
+    print('Now, find all other combinations.')
+    print(aw.all_wordifications(w2n.words_to_number(cell)))
+    print(len(aw.all_wordifications(w2n.words_to_number(cell))))
+    input('To go to next case, hit enter...')
